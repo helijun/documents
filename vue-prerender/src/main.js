a9dcb7c9d2d83e2e996d0092613fc948531cc3d6
何立军 <anarkhz@tom.com>
2018-10-26 15:20:25 +0800

    coding

--- vue-prerender/src/main.js
+++ vue-prerender/src/main.js
@@ -3,45 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
 Vue.$router = router
-
-Vue.mixin({
-  data () {
-    return {
-      dataReady: false
-    }
-  }
-})
-
-// 注册全局自定义指令
-Vue.directive('min-height', {
-  // 当绑定元素插入到 DOM 中。
-  inserted: function (el) {
-    let minHeight = window.innerHeight; //el.parentElement.clientHeight 
-    if(minHeight) {
-      el.style.minHeight = minHeight + 'px'
-    }
-  }
-})
-
-// 返回事件监听
-const onBack = {}
-onBack.install = function(_Vue, options) {
-  // 注入组件
-  _Vue.mixin({
-    created () {
-      if (this.$options.onBack) {
-        console.log('执行onBack')
-        this.$options.onBack.call(this)
-      }
-    }    
-  })
-}
-Vue.use(onBack)
-
-
+Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
